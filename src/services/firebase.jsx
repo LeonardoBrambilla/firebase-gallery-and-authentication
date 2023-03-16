643b7f580bbf45f4b39678b47360a5d2cb1fb7d7
Leonardo <kjamanba@hotmail.com>
2023-03-16 11:29:09 -0300

    test

--- src/services/firebase.jsx
+++ src/services/firebase.jsx
@@ -3,13 +3,13 @@ import {getAuth} from "firebase/auth"
 import {getStorage} from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBeLkEDtkw3GV8vWyDtkG4aS8S8ggAT4ys",
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: "auth-30411",
-  storageBucket: "auth-30411.appspot.com",
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: ,
+  authDomain: ,
+  databaseURL: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: 
 }
 
 const app = initializeApp(firebaseConfig)
